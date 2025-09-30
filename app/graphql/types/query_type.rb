1f4f519feb98e4997952e02670bf88c7f46a61ab
Rohith Poyyeri <rohithart@yahoo.com>
2025-09-30 11:50:03 +1000

    fix: Query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,11 +2,13 @@ module Types
   class QueryType < Types::BaseObject
     field :users, [Types::UserType], null: false do
       argument :min_orders, Integer, required: false
+      argument :email_domain, String, required: false
     end
 
-    def users(min_orders: nil)
-      # TODO: Implement resolver, optionally filtering by min_orders
-      User.all
+    def users(min_orders: nil, email_domain: nil)
+      relation = min_orders.present? ? User.with_order_counts(min: min_orders, since: 30.days.ago) : User.all
+      relation = relation.where(email_domain: email_domain.downcase) if email_domain.present?
+      relation
     end
   end
 end
